62696eb4b3e9c27e4c16a03a9b1e527f4b479acc
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-04 15:28:49 -0700

    remove class-app remnants

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,25 +13,6 @@ export class HomeComponent implements OnInit {
   subTitle = 'Yet Another Reflection App'
   isAuth: boolean;
 
-  budget = '7000000000';
-
-  heroes = [{
-    name: 'Captain Marvel',
-    rent: '2000000000'
-  }, 
-  {
-    name: 'Spiderman',
-    rent: 100000
-  }, 
-  {
-    name: 'Black Panther',
-    rent: '1000000000'
-  }, 
-  {
-    name: 'Thor',
-    rent: '2000000000'
-  }];
-
   entries = [
     new JournalEntry(
       Guid.create(),
