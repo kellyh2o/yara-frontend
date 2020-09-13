a0e06bcb451b2287126d49474b2e3fd7210b4cc4
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-12 18:03:10 -0700

    aaand more unit tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,13 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+
 import { HomeComponent } from './home.component';
-import { Guid } from "guid-typescript";
+
+import { MemoizedSelector } from '@ngrx/store';
+import { JournalState } from '../store/state/journal-state.model';
+import { getShowEntries, getShowNewEntryForm } from '../store';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let mockStore: MockStore;
+  let mockGetShowEntriesSelector: MemoizedSelector<JournalState, boolean>;
+  let mockGetShowNewEntrySelector: MemoizedSelector<JournalState, boolean>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: [provideMockStore()],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
@@ -15,7 +24,53 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
+    mockStore = TestBed.inject(MockStore);
+    mockGetShowEntriesSelector = mockStore.overrideSelector(getShowEntries, false);
+    mockGetShowNewEntrySelector = mockStore.overrideSelector(getShowNewEntryForm, false);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+
+  it('showEntries should return true if getShowEntries returns true', (done) => {
+    mockGetShowEntriesSelector.setResult(true);
+    mockStore.refreshState();
+    fixture.detectChanges();
+    component.showEntries$.subscribe((showEntries) => {
+      expect(showEntries).toBeTruthy();
+      done();
+    });
+  });
+
+  it('showEntries should return false if getShowEntries returns false', (done) => {
+    mockGetShowEntriesSelector.setResult(false);
+    mockStore.refreshState();
+    fixture.detectChanges();
+    component.showEntries$.subscribe((showEntries) => {
+      expect(showEntries).toBeFalsy();
+      done();
+    });
+  });
+
+  it('showNewEntryForm should return true if getShowNewEntryForm returns true', (done) => {
+    mockGetShowNewEntrySelector.setResult(true);
+    mockStore.refreshState();
+    fixture.detectChanges();
+    component.showNewEntryForm$.subscribe((showEntries) => {
+      expect(showEntries).toBeTruthy();
+      done();
+    });
+  });
+
+  it('showNewEntryForm should return false if getShowNewEntryForm returns false', (done) => {
+    mockGetShowNewEntrySelector.setResult(false);
+    mockStore.refreshState();
+    fixture.detectChanges();
+    component.showNewEntryForm$.subscribe((showEntries) => {
+      expect(showEntries).toBeFalsy();
+      done();
+    });
+  });
+
 });
+
