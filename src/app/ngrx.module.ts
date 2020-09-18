9eb22de014ae2ab07d204a1913b4de766efc9761
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-18 11:48:50 -0700

    login works now

--- src/app/ngrx.module.ts
+++ src/app/ngrx.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
-import { appState } from './store';
+import { appState, AuthEffects } from './store';
 
 @NgModule({
   declarations: [],
   imports: [
     StoreModule.forRoot(appState),
     StoreDevtoolsModule.instrument({ maxAge: false }),
+    EffectsModule.forRoot([AuthEffects])
   ],
 })
 export class NgrxModule {}
