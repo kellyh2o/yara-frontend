c47279e4a600a50e81506d065d70caed658f5915
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-19 19:07:59 -0700

    more unit tests

--- src/app/store/selectors/auth.selectors.spec.ts
+++ src/app/store/selectors/auth.selectors.spec.ts
@@ -1,4 +1,4 @@
-import { getIsAuth, getToken } from './auth.selectors';
+import { getIsAuth, getToken, getShowLogin } from './auth.selectors';
 import { AUTH_INITIAL_MOCK_STATE } from '../state/auth-initial-mock-state';
 
 const state = AUTH_INITIAL_MOCK_STATE;
@@ -6,6 +6,10 @@ const state = AUTH_INITIAL_MOCK_STATE;
 describe('Auth selectors', () => {
   it('should retrieve authState', () => {
     expect(getIsAuth.projector(state)).toBe(state.isAuth);
+  });
+
+  it('should retrieve getShowLogin', () => {
+    expect(getShowLogin.projector(state)).toBe(!state.isAuth);
   });
 
   it('should retrieve token from authState', () => {
