e2a2e8e403cd08607022d90a0a7fc072953f1700
Kelly Watermeyer <kellywatermeyer@gmail.com>
2020-09-26 16:03:02 -0700

    more unit tests

--- src/app/create-reflection/create-reflection.component.spec.ts
+++ src/app/create-reflection/create-reflection.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
-import { provideMockStore } from '@ngrx/store/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 import { CreateReflectionComponent } from './create-reflection.component';
+import { ApplicationState } from '../store/models/application-state.model';
+import { Mock } from 'protractor/built/driverProviders';
+import { saveNewReflection, cancelNewReflection } from '../store';
 
 describe('CreateReflectionComponent', () => {
   let component: CreateReflectionComponent;
   let fixture: ComponentFixture<CreateReflectionComponent>;
+  let store: MockStore<ApplicationState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,10 +22,21 @@ describe('CreateReflectionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateReflectionComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should dispatch saveNewReflection on submit', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    const action = saveNewReflection();
+    component.submit();
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
+  });
+
+  it('should dispatch cancelNewReflection on cancel', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    const action = cancelNewReflection();
+    component.submit();
+    expect(dispatchSpy).toHaveBeenCalledWith(action);
   });
 });
